7f6a6b45dc02314901f89adec97a8ff0eec3dcdf
joaovitorlopes <jvlaaa+github@gmail.com>
2024-05-13 11:07:14 -0400

    first commit

---
+++ src/main/java/joaovitorlopes/com/github/searchcarbyfipe/SearchCarByFipeApplication.java
@@ -0,0 +1,13 @@
+package joaovitorlopes.com.github.searchcarbyfipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchCarByFipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchCarByFipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/joaovitorlopes/com/github/searchcarbyfipe/SearchCarByFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package joaovitorlopes.com.github.searchcarbyfipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchCarByFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
